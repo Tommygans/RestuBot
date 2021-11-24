4b21106640fc5ccf09df270c76547208c870ddab
Tommygans <91420132+Tommygans@users.noreply.github.com>
2021-11-24 16:06:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 global.DeveloperMode = 'false' //true Or false
-global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://chat.whatsapp.com/FrluVyY5kBRIhY6yKfH4DI']
-global.owner = ['6285783417029'] // Owner
-global.mods = [] // moderator
-global.prems = []  //premium
+global.linkGC = ['https://chat.whatsapp.com/GfNO7PL6rsS4iSGEXSELzN', 'https://chat.whatsapp.com/FyeK866EprW9KQQGuhb50b']
+global.owner = ['6285780589674'] // Owner
+global.mods = ['6285780589674'] // moderator
+global.prems = ['6285780589674']  //premium
 global.APIs = { // API Prefix
   // name: 'https://website'
   rns: 'https://api-reysekha.herokuapp.com',
@@ -23,24 +23,24 @@ global.APIs = { // API Prefix
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
   'https://api-reysekha.herokuapp.com': 'apirey',
-  'https://api.xteam.xyz': 'APIKEY_DISINI',
+  'https://api.xteam.xyz': 'fe63715981b9aa98',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
   'https://bsbt-api-rest.herokuapp.com': 'benniismael',
   'https://api.zeks.xyz': 'apivinz',
   'https://pencarikode.xyz': 'pais',
   'https://hardianto-chan.herokuapp.com': 'hardianto',
   'https://leyscoders-api.herokuapp.com': 'MIMINGANZ',
-  'https://api.lolhuman.xyz': 'APIKEY_DISINI',
-  'https://api.chipa.xyz': 'APIKEY_DISINI'
+  'https://api.lolhuman.xyz': 'a09437f1c51d60f974f4e8bc',
+  'https://api.chipa.xyz': '70K62AZFY8EGFUDDCNL4ZOVW'
 }
 
 // Sticker WM
-global.packname = 'Create By RestuBot\nOwner : Muhammad Restu'
+global.packname = 'Create By Tommy\nOwner : Muhammad Tommy'
 global.author = ''
 
 global.multiplier = 39 // The higher, The harder levelup
 
-global.botwm = "©RestuBot" // Wm
+global.botwm = "©TommyBot" // Wm
 
 let fs = require('fs')
 let chalk = require('chalk')
